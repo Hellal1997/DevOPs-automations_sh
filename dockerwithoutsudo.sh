5b07bd288e368f5957312b9b88d70f7463c28993
mohamed hellal <153162650+Hellal1997@users.noreply.github.com>
2024-08-21 03:35:58 +0300

    Create dockerwithoutsudo.sh

---
+++ dockerwithoutsudo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Update package manager
+sudo apt update
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# Add current user to the docker group
+sudo usermod -aG docker $USER
+
+# Restart Docker daemon
+sudo systemctl restart docker
